e522540e6b4f995c0cbb6738796e281c4cbe1fa7
riasat01 <riasat01@outlook.com>
2024-11-30 20:23:04 +0600

    used request handler of express in user module

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,42 +1,44 @@
-import { NextFunction, Request, Response } from 'express';
+import { RequestHandler } from 'express';
 // import ZodStudentSchema from '../student/zod.student.model';
 import { UserServices } from './user.service';
+import sendResponse from '../../utils/sendResponse';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
+const createStudent: RequestHandler = async (
+    req,
+    res,
+    next,
 ) => {
-  try {
-    const { password, student } = req.body;
+    try {
+        const { password, student } = req.body;
 
-    // Joi validation
-    // const { error, value } = JoiStudentSchema.validate(student);
-    // console.log(error, value);
-    // if (error || !value) {
-    //   res.status(500).json({
-    //     succcess: false,
-    //     message: `something went wrong while creating student`,
-    //     error,
-    //   });
-    // }
+        // Joi validation
+        // const { error, value } = JoiStudentSchema.validate(student);
+        // console.log(error, value);
+        // if (error || !value) {
+        //   res.status(500).json({
+        //     succcess: false,
+        //     message: `something went wrong while creating student`,
+        //     error,
+        //   });
+        // }
 
-    // zod validation
+        // zod validation
 
-    // const zodParsedData = ZodStudentSchema.parse(student);
-    // console.log(zodParsedData);
-    // const result = await UserServices.createStudentIntoDb(zodParsedData);
-    const result = await UserServices.createStudentIntoDb(password, student);
-    res.status(200).json({
-      success: true,
-      message: `student created successfully`,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
+        // const zodParsedData = ZodStudentSchema.parse(student);
+        // console.log(zodParsedData);
+        // const result = await UserServices.createStudentIntoDb(zodParsedData);
+        const result = await UserServices.createStudentIntoDb(password, student);
+        sendResponse(res, {
+            statusCode: 200,
+            success: true,
+            message: `student created successfully`,
+            data: result
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 export const UserController = {
-  createStudent,
+    createStudent,
 };
