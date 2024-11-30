44592a491e61eed8fbd7ff6ea1cc8f13ef56eb24
riasat01 <riasat01@outlook.com>
2024-11-30 20:53:36 +0600

    student controllers updated to follow DRY pattern

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,46 +1,46 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { StudentServices } from './student.service';
+import sendResponse from '../../utils/sendResponse';
 // import JoiStudentSchema from './joi.student.model';
 
-const getAllStudents: RequestHandler = async (req, res, next) => {
-  try {
-    const result = await StudentServices.getAllStudentsFromDb();
-    res.status(200).json({
-      success: true,
-      message: `students are retrieved successfully`,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
+const catchAsync = async (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((error) => next(error));
+  };
 };
 
-const getSingleStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await StudentServices.getSingleStudentFromDb(id);
-    res.status(200).json({
-      success: true,
-      message: `student is retrieved successfully`,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-const deleteSingleStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await StudentServices.deleteSingleStudentFromDb(id);
-    res.status(200).json({
-      success: true,
-      message: `student is deleted successfully`,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+const getAllStudents = catchAsync(async (req, res, next) => {
+  const result = await StudentServices.getAllStudentsFromDb();
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: `students are retrieved successfully`,
+    data: result,
+  });
+});
+
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+const getSingleStudent = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const result = await StudentServices.getSingleStudentFromDb(id);
+  res.status(200).json({
+    success: true,
+    message: `student is retrieved successfully`,
+    data: result,
+  });
+});
+
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+const deleteSingleStudent = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const result = await StudentServices.deleteSingleStudentFromDb(id);
+  res.status(200).json({
+    success: true,
+    message: `student is deleted successfully`,
+    data: result,
+  });
+});
 
 export const Studentcontroller = {
   getAllStudents,
