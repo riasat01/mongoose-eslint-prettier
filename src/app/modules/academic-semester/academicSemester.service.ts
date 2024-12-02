6d1963900b75e459679616f419e64e8f325e8a1a
riasat01 <riasat01@outlook.com>
2024-12-02 17:27:02 +0600

    feat: get all, get single, update semester feature added

--- src/app/modules/academic-semester/academicSemester.service.ts
+++ src/app/modules/academic-semester/academicSemester.service.ts
@@ -10,6 +10,37 @@ const createAcademicSemesterIntoDb = async (payload: IAcademicSemester) => {
   return result;
 };
 
+const getAllAcademicSemestersFromDB = async () => {
+  const result = await AcademicSemester.find();
+  return result;
+};
+
+const getSingleAcademicSemesterFromDB = async (id: string) => {
+  const result = await AcademicSemester.findById(id);
+  return result;
+};
+
+const updateAcademicSemesterIntoDB = async (
+  id: string,
+  payload: Partial<IAcademicSemester>,
+) => {
+  if (
+    payload.name &&
+    payload.code &&
+    AcademicSemesterCodeMapper[payload.name] !== payload.code
+  ) {
+    throw new Error('Invalid Semester Code');
+  }
+
+  const result = await AcademicSemester.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
+
 export const AcademicSemesterServices = {
   createAcademicSemesterIntoDb,
+  getAllAcademicSemestersFromDB,
+  getSingleAcademicSemesterFromDB,
+  updateAcademicSemesterIntoDB,
 };
