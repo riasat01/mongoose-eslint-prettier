5ae90ad0433e0994655e558665343dc0d1ebe183
riasat01 <riasat01@outlook.com>
2024-12-04 09:06:23 +0600

    fix: student id resetting on newly started semester related issue fixed

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -15,11 +15,24 @@ const findLastStudentId = async() => {
           createdAt: -1,
         })
         .lean();
-    return lastStudent?.id ? lastStudent.id.substring(6, 10) : undefined
+    return lastStudent?.id ? lastStudent.id : undefined
 }
 
 export const generateStudentId = async (payload: IAcademicSemester) => {
-    const currentId: string = await findLastStudentId() || (0).toString();
+    const lastStudentId: string | undefined = await findLastStudentId();
+    const currentYear : string = payload.year.getFullYear().toString();
+    const currentStudentSemesterCode: string = payload.code;
+    let lastStudentSemesterCode: string = "";
+    let lastStudentYear: string = "";
+    let currentId: string = (0).toString();
+    if(lastStudentId){
+      lastStudentSemesterCode = lastStudentId.substring(4,6);
+      lastStudentYear = lastStudentId.substring(0,4);
+    }
+    if(lastStudentId && lastStudentSemesterCode === currentStudentSemesterCode && lastStudentYear === currentYear){
+        currentId = lastStudentId.substring(6, 10);
+    }
+    
     let incrementId: string = (Number(currentId) + 1).toString().padStart(4, '0');
     incrementId = `${(payload.year).getFullYear()}${payload?.code}${incrementId};`
     return incrementId;
