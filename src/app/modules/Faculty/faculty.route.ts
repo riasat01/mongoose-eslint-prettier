3dfca3bb03b707e467ab507a376e99086b949642
riasat01 <riasat01@outlook.com>
2024-12-10 10:50:19 +0600

    feat: add faculty route

---
+++ src/app/modules/Faculty/faculty.route.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { updateFacultyValidationSchema } from './faculty.validation';
+import { FacultyControllers } from './faculty.controller';
+
+const router = Router();
+
+router.get('/:id', FacultyControllers.getSingleFaculty);
+
+router.patch(
+  '/:id',
+  validateRequest(updateFacultyValidationSchema),
+  FacultyControllers.updateFaculty,
+);
+
+router.delete('/:id', FacultyControllers.deleteFaculty);
+
+router.get('/', FacultyControllers.getAllFaculties);
+
+export const FacultyRoutes = router;
