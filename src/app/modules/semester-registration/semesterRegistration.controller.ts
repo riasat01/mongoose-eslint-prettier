82b5fb64cd131b6aa281385d3c219f7c6b1d3646
riasat01 <riasat01@outlook.com>
2024-12-13 21:13:46 +0600

    feat: create routes for semester registration

--- src/app/modules/semester-registration/semesterRegistration.controller.ts
+++ src/app/modules/semester-registration/semesterRegistration.controller.ts
@@ -4,78 +4,76 @@ import sendResponse from '../../utils/sendResponse';
 import { SemesterRegistrationService } from './semesterRegistration.service';
 
 const createSemesterRegistration = catchAsync(
-    async (req: Request, res: Response) => {
-        const result =
-            await SemesterRegistrationService.createSemesterRegistrationIntoDB(
-                req.body,
-            );
+  async (req: Request, res: Response) => {
+    const result =
+      await SemesterRegistrationService.createSemesterRegistrationIntoDB(
+        req.body,
+      );
 
-        sendResponse(res, {
-            statusCode: 200,
-            success: true,
-            message: 'Semester Registration is created successfully!',
-            data: result,
-        });
-    },
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: 'Semester Registration is created successfully!',
+      data: result,
+    });
+  },
 );
 
 const getAllSemesterRegistrations = catchAsync(
-    async (req: Request, res: Response) => {
-        const result =
-            await SemesterRegistrationService.getAllSemesterRegistrationsFromDB(
-                req.query,
-            );
+  async (req: Request, res: Response) => {
+    const result =
+      await SemesterRegistrationService.getAllSemesterRegistrationsFromDB(
+        req.query,
+      );
 
-        sendResponse(res, {
-            statusCode: 200,
-            success: true,
-            message: 'Semester Registration is retrieved successfully !',
-            data: result,
-        });
-    },
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: 'Semester Registration is retrieved successfully !',
+      data: result,
+    });
+  },
 );
 
 const getSingleSemesterRegistration = catchAsync(
-    async (req: Request, res: Response) => {
-        const { id } = req.params;
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-        const result =
-            await SemesterRegistrationService.getSingleSemesterRegistrationsFromDB(
-                id,
-            );
+    const result =
+      await SemesterRegistrationService.getSingleSemesterRegistrationsFromDB(
+        id,
+      );
 
-        sendResponse(res, {
-            statusCode: 200,
-            success: true,
-            message: 'Semester Registration is retrieved successfully',
-            data: result,
-        });
-    },
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: 'Semester Registration is retrieved successfully',
+      data: result,
+    });
+  },
 );
 
 const updateSemesterRegistration = catchAsync(
-    async (req: Request, res: Response) => {
-        const { id } = req.params;
-        const result =
-            await SemesterRegistrationService.updateSemesterRegistrationIntoDB(
-                id,
-                req.body,
-            );
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result =
+      await SemesterRegistrationService.updateSemesterRegistrationIntoDB(
+        id,
+        req.body,
+      );
 
-        sendResponse(res, {
-            statusCode: 200,
-            success: true,
-            message: 'Semester Registration is updated successfully',
-            data: result,
-        });
-    },
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: 'Semester Registration is updated successfully',
+      data: result,
+    });
+  },
 );
 
-
-
 export const SemesterRegistrationController = {
-    createSemesterRegistration,
-    getAllSemesterRegistrations,
-    getSingleSemesterRegistration,
-    updateSemesterRegistration,
+  createSemesterRegistration,
+  getAllSemesterRegistrations,
+  getSingleSemesterRegistration,
+  updateSemesterRegistration,
 };
