91ab23c2ac9bea944cc4681619852fce73583b58
riasat01 <riasat01@outlook.com>
2024-12-13 09:06:42 +0600

    feat: create semester registration model

--- src/app/modules/semester-registration/semesterRegistration.interface.ts
+++ src/app/modules/semester-registration/semesterRegistration.interface.ts
@@ -1,10 +1,10 @@
 import { Types } from 'mongoose';
 
 export interface ISemesterRegistration {
-    academicSemester: Types.ObjectId;
-    status: 'UPCOMING' | 'ONGOING' | 'ENDED';
-    startDate: Date;
-    endDate: Date;
-    minCredit: number;
-    maxCredit: number;
-};
+  academicSemester: Types.ObjectId;
+  status: 'UPCOMING' | 'ONGOING' | 'ENDED';
+  startDate: Date;
+  endDate: Date;
+  minCredit: number;
+  maxCredit: number;
+}
