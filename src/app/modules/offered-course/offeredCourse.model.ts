805816b399baf8a958749c48cc17d4bc8626dbb6
riasat01 <riasat01@outlook.com>
2024-12-13 21:34:08 +0600

    feat: create offered course model

---
+++ src/app/modules/offered-course/offeredCourse.model.ts
@@ -0,0 +1,68 @@
+import mongoose, { Schema } from 'mongoose';
+import { Days } from './offeredCourse.constant';
+import { TOfferedCourse } from './OfferedCourse.interface';
+
+const offeredCourseSchema = new mongoose.Schema<TOfferedCourse>(
+  {
+    semesterRegistration: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Semester-Registration',
+    },
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'academic-semester',
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'AcademicFaculty',
+    },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Academic-Department',
+    },
+    course: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Course',
+    },
+    faculty: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Faculty',
+    },
+    maxCapacity: {
+      type: Number,
+      required: true,
+    },
+    section: {
+      type: Number,
+      required: true,
+    },
+    days: [
+      {
+        type: String,
+        enum: Days,
+      },
+    ],
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const OfferedCourse = mongoose.model<TOfferedCourse>(
+  'OfferedCourse',
+  offeredCourseSchema,
+);
